5df24fbcd0142d2a170260ab9f90df27bda2ea3b
ansaws <junk.ansaws@gmail.com>
2020-06-21 13:14:33 -0500

    created console app

---
+++ SecurityProjectConsole/SecurityProjectConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecurityProjectConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
