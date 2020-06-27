80ae79388a6dea1a42d9a938cb8a06f7cf723d94
ShivanshV <36553424+ShivanshV@users.noreply.github.com>
2020-06-27 11:52:07 -0500

    Started Beginning

--- SecurityProjectConsole/SecurityProjectConsole/Program.cs
+++ SecurityProjectConsole/SecurityProjectConsole/Program.cs
@@ -8,7 +8,24 @@ namespace SecurityProjectConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Welcome, Do you already have an account created? Y/N");
+            string RegisterOrLogin = Console.ReadLine();
+            if (RegisterOrLogin == "Y")
+            {
+                Console.Clear();
+
+            }
+            else if (RegisterOrLogin == "N")
+            {
+                Console.Clear();
+                Console.WriteLine("REGISTER \n");
+                Console.WriteLine("Username:");
+                string newUsername = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Response. Please write either Y or N.");
+            }
             //This is a change
         }
     }
