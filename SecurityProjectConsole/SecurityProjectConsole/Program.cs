cce39793bb9b0d71a2a3338a0f35f834c532ba16
ansaws <junk.ansaws@gmail.com>
2020-06-28 12:57:17 -0500

    Update Program.cs

--- SecurityProjectConsole/SecurityProjectConsole/Program.cs
+++ SecurityProjectConsole/SecurityProjectConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Text.RegularExpressions;
 
@@ -21,15 +23,31 @@ namespace SecurityProjectConsole
             else if (RegisterOrLogin == "N" || RegisterOrLogin == "n")
             {
                 Console.Clear();
+                List<String> data register();/*
                 Console.WriteLine("REGISTER \n");
                 Console.WriteLine("Username:");
-                string newUsername = Console.ReadLine();
+                string newUsername = Console.ReadLine();*/
             }
             else
             {
                 Console.WriteLine("Invalid Response. Please write either Y or N.");
             }
             //This is a change
+        }
+
+        private List<String> register()
+        {
+            List<String> userData = new List<String>();
+            Console.WriteLine("Username: ");
+            userData.Add(Console.ReadLine());
+            Console.WriteLine("Password: ");
+            userData.Add(Console.ReadLine());
+            Console.WriteLine("What is your favorite animal: ");
+            userData.Add(Console.ReadLine());
+            Console.WriteLine("Phone: ");
+            userData.Add(Console.ReadLine());
+            return userData;
+            
         }
     }
 }
